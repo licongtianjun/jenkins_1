5265f3cf4450db07e6d4a79ab74bfc432a5b9a67
licong <805903194@qq.com>
2020-03-09 23:31:48 +0800

    appium_1.py1111111

---
+++ appium_1.py
@@ -0,0 +1,47 @@
+from appium import webdriver
+import time
+import unittest
+from appium.webdriver.common.touch_action import TouchAction
+
+class Ceshi(unittest.TestCase):
+    def setUp(self):
+        self.caps ={
+          "platformName": "Android",
+          "deviceName": "sagit",
+          "appPackage": "com.alibaba.android.rimet",
+          "appActivity": ".biz.SplashActivity"
+        }
+    def test_1(self):
+        driver = webdriver.Remote("http://localhost:4723/wd/hub",self.caps)
+        TouchAction(driver).press(x=555, y=1846).move_to(x=588, y=876).release().perform()
+        driver.implicitly_wait(10)
+        driver.find_element_by_id("android:id/button1").click()
+        driver.find_element_by_id("android:id/button1").click()
+        driver.find_element_by_id("com.alibaba.android.rimet:id/login_slide_btn").click()
+        driver.find_element_by_id("com.alibaba.android.rimet:id/et_phone_input").clear()
+        driver.find_element_by_id("com.alibaba.android.rimet:id/et_phone_input").send_keys("18510860419")
+        driver.find_element_by_id("com.alibaba.android.rimet:id/et_pwd_login").send_keys("licong12023")
+        driver.find_element_by_id('com.alibaba.android.rimet:id/btn_next').click()
+        time.sleep(7)
+        TouchAction(driver).tap(x=960, y=1832).perform()
+        aa = driver.find_element_by_id('android:id/message').text
+        self.assertEqual(aa,'号码或密码错误，请重新输入','账号密码不对时,测试没通过')
+    def test_2(self):
+        driver = webdriver.Remote("http://localhost:4723/wd/hub", self.caps)
+        TouchAction(driver).press(x=555, y=1846).move_to(x=588, y=876).release().perform()
+        driver.implicitly_wait(10)
+        driver.find_element_by_id("android:id/button1").click()
+        driver.find_element_by_id("android:id/button1").click()
+        driver.find_element_by_id("com.alibaba.android.rimet:id/login_slide_btn").click()
+        driver.find_element_by_id("com.alibaba.android.rimet:id/et_phone_input").clear()
+        driver.find_element_by_id("com.alibaba.android.rimet:id/et_phone_input").send_keys("18510860419")
+        driver.find_element_by_id("com.alibaba.android.rimet:id/et_pwd_login").send_keys("licong1202")
+        driver.find_element_by_id('com.alibaba.android.rimet:id/btn_next').click()
+        time.sleep(7)
+        TouchAction(driver).tap(x=960, y=1832).perform()
+        time.sleep(10)
+        aa = driver.find_element_by_id('android:id/message').text
+        self.assertNotEqual(aa, '号码或密码错误，请重新输入', '账号密码一致时,测试没通过')
+
+if __name__ == '__main__':
+    unittest.main()
